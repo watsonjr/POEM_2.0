b22028d50cbebbeeb6cc0732eccf807119543600
Colleen Petrik <colleenpetrik@gmail.com>
2016-08-16 16:22:36 -0500

    Test JW advect time steps

---
+++ CODE/Figs/poem_mfiles/vis_JWadvec_test.m
@@ -0,0 +1,314 @@
+% Visualize advection test cases
+
+clear all
+close all
+
+dpath = '/Users/cpetrik/Dropbox/Princeton/POEM_2.0/CODE/Data/CSV/advect_tests/';
+fpath = '/Users/cpetrik/Dropbox/Princeton/POEM_2.0/CODE/Figs/PNG/advect_tests/';
+
+bio = csvread([dpath 'bio_JWadvect_test_Atl_noswim_6hr.csv']);
+cname = 'Atl_noswim_6hr';
+
+grid = csvread('grid_csv.csv');
+load('gridspec_forecast.mat');
+
+%% Conservation of mass
+
+totb = sum(bio,2);
+figure(11)
+%subplot(2,2,1)
+plot(totb)
+100*(totb(1)-totb(end))/totb(1)
+%           dt=1d       dt=6hr        
+%Eq =   
+%Pac =  
+%Atl =      -5.7e+95%   NaN
+%Arctic = 
+%Antarc = 
+%WInd =                      
+%EInd =             
+
+
+%%
+B1=NaN*ones(size(geolat_t));
+B2=B1;
+B3=B1;
+B4=B1;
+B5=B1;
+
+%for n=1:size(bio,1);
+    B1(grid(:,1))=bio(1,:);
+    B2(grid(:,1))=bio(73,:); %73
+    B3(grid(:,1))=bio(146,:); %146
+    B4(grid(:,1))=bio(219,:); %219
+    B5(grid(:,1))=bio(366,:); %365
+%end
+
+%% plot info
+% Land
+surf_tmask = tmask(:,:,1);
+lmask = surf_tmask;
+lmask(lmask==0) = 999;
+lmask(lmask==1) = NaN;
+
+%axes labels
+xt = -250:50:50;
+xl = xt;
+xl(xl<-180) = xl(xl<-180) + 350;
+
+
+%% Start
+figure(11)
+% Create two axes
+ax1 = axes;
+pcolor(ax1,geolon_t,geolat_t,lmask); hold on;
+shading flat
+title('Day=0')
+view(2)
+ax2 = axes;
+pcolor(ax2,geolon_t,geolat_t,B1); hold on;
+shading flat
+% Link them together
+linkaxes([ax1,ax2])
+% Hide the top axes
+ax2.Visible = 'off';
+ax2.XTick = [];
+ax2.YTick = [];
+% Give each one its own colormap
+colormap(ax1,'gray')
+colormap(ax2,'jet')
+caxis([0 10])
+% Then add colorbars and get everything lined up
+set([ax1,ax2],'Position',[.1 .11 .75 .815]);
+cb2 = colorbar(ax2,'Position',[.88 .11 .0675 .815]);
+set(ax1,'XTick',xt,'XTickLabel',xl)
+print('-dpng',[fpath 'advec_test_' cname '_1.png'])
+
+%
+figure(12)
+% Create two axes
+ax1 = axes;
+pcolor(ax1,geolon_t,geolat_t,lmask); hold on;
+shading flat
+title('Day=73') %73
+view(2)
+ax2 = axes;
+pcolor(ax2,geolon_t,geolat_t,B2); hold on;
+shading flat
+% Link them together
+linkaxes([ax1,ax2])
+% Hide the top axes
+ax2.Visible = 'off';
+ax2.XTick = [];
+ax2.YTick = [];
+% Give each one its own colormap
+colormap(ax1,'gray')
+colormap(ax2,'jet')
+caxis([0 10])
+% Then add colorbars and get everything lined up
+set([ax1,ax2],'Position',[.1 .11 .75 .815]);
+cb2 = colorbar(ax2,'Position',[.88 .11 .0675 .815]);
+set(ax1,'XTick',xt,'XTickLabel',xl)
+print('-dpng',[fpath 'advec_test_' cname '_2.png'])
+
+figure(13)
+% Create two axes
+ax1 = axes;
+pcolor(ax1,geolon_t,geolat_t,lmask); hold on;
+shading flat
+title('Day=146') 
+view(2)
+ax2 = axes;
+pcolor(ax2,geolon_t,geolat_t,B3); hold on;
+shading flat
+% Link them together
+linkaxes([ax1,ax2])
+% Hide the top axes
+ax2.Visible = 'off';
+ax2.XTick = [];
+ax2.YTick = [];
+% Give each one its own colormap
+colormap(ax1,'gray')
+colormap(ax2,'jet')
+caxis([0 10])
+% Then add colorbars and get everything lined up
+set([ax1,ax2],'Position',[.1 .11 .75 .815]);
+cb2 = colorbar(ax2,'Position',[.88 .11 .0675 .815]);
+set(ax1,'XTick',xt,'XTickLabel',xl)
+print('-dpng',[fpath 'advec_test_' cname '_3.png'])
+
+figure(14)
+% Create two axes
+ax1 = axes;
+pcolor(ax1,geolon_t,geolat_t,lmask); hold on;
+shading flat
+title('Day=219') %219
+view(2)
+ax2 = axes;
+pcolor(ax2,geolon_t,geolat_t,B4); hold on;
+shading flat
+% Link them together
+linkaxes([ax1,ax2])
+% Hide the top axes
+ax2.Visible = 'off';
+ax2.XTick = [];
+ax2.YTick = [];
+% Give each one its own colormap
+colormap(ax1,'gray')
+colormap(ax2,'jet')
+caxis([0 10])
+% Then add colorbars and get everything lined up
+set([ax1,ax2],'Position',[.1 .11 .75 .815]);
+cb2 = colorbar(ax2,'Position',[.88 .11 .0675 .815]);
+set(ax1,'XTick',xt,'XTickLabel',xl)
+print('-dpng',[fpath 'advec_test_' cname '_4.png'])
+
+%
+figure(15)
+% Create two axes
+ax1 = axes;
+pcolor(ax1,geolon_t,geolat_t,lmask); hold on;
+shading flat
+title('Day=365')
+view(2)
+ax2 = axes;
+pcolor(ax2,geolon_t,geolat_t,B5); hold on;
+shading flat
+% Link them together
+linkaxes([ax1,ax2])
+% Hide the top axes
+ax2.Visible = 'off';
+ax2.XTick = [];
+ax2.YTick = [];
+% Give each one its own colormap
+colormap(ax1,'gray')
+colormap(ax2,'jet')
+caxis([0 10])
+% Then add colorbars and get everything lined up
+set([ax1,ax2],'Position',[.1 .11 .75 .815]);
+cb2 = colorbar(ax2,'Position',[.88 .11 .0675 .815]);
+set(ax1,'XTick',xt,'XTickLabel',xl)
+print('-dpng',[fpath 'advec_test_' cname '_5.png'])
+
+%% Arctic projection
+figure(16)
+m_proj('stereographic','lat',90,'long',30,'radius',30);
+m_pcolor(geolon_t,geolat_t,B1);
+shading flat
+colorbar
+colormap('jet')
+caxis([0 1e4])
+title('Day=1')
+m_grid('xtick',12,'tickdir','out','ytick',[70 80],'linest','-');
+m_coast('patch',[.7 .7 .7],'edgecolor','k');
+%print('-dpng',[fpath 'advec_test_' cname '_1_arcticproj.png'])
+
+figure(17)
+m_proj('stereographic','lat',90,'long',30,'radius',30);
+m_pcolor(geolon_t,geolat_t,B2);
+shading flat
+colorbar
+colormap('jet')
+caxis([0 1e4])
+title('Day=73')
+m_grid('xtick',12,'tickdir','out','ytick',[70 80],'linest','-');
+m_coast('patch',[.7 .7 .7],'edgecolor','k');
+%print('-dpng',[fpath 'advec_test_' cname '_2_arcticproj.png'])
+
+figure(18)
+m_proj('stereographic','lat',90,'long',30,'radius',30);
+m_pcolor(geolon_t,geolat_t,B3);
+shading flat
+colorbar
+colormap('jet')
+caxis([0 1e4])
+title('Day=146')
+m_grid('xtick',12,'tickdir','out','ytick',[70 80],'linest','-');
+m_coast('patch',[.7 .7 .7],'edgecolor','k');
+%print('-dpng',[fpath 'advec_test_' cname '_3_arcticproj.png'])
+
+figure(19)
+m_proj('stereographic','lat',90,'long',30,'radius',30);
+m_pcolor(geolon_t,geolat_t,B4);
+shading flat
+colorbar
+colormap('jet')
+caxis([0 1e4])
+title('Day=219')
+m_grid('xtick',12,'tickdir','out','ytick',[70 80],'linest','-');
+m_coast('patch',[.7 .7 .7],'edgecolor','k');
+%print('-dpng',[fpath 'advec_test_' cname '_4_arcticproj.png'])
+
+figure(20)
+m_proj('stereographic','lat',90,'long',30,'radius',30);
+m_pcolor(geolon_t,geolat_t,B5);
+shading flat
+colorbar
+colormap('jet')
+caxis([0 1e4])
+title('Day=365')
+m_grid('xtick',12,'tickdir','out','ytick',[70 80],'linest','-');
+m_coast('patch',[.7 .7 .7],'edgecolor','k');
+%print('-dpng',[fpath 'advec_test_' cname '_5_arcticproj.png'])
+
+%% Antarctic projection
+figure(26)
+m_proj('stereographic','lat',-90,'long',30,'radius',50);
+m_pcolor(geolon_t,geolat_t,B1);
+shading flat
+colorbar
+colormap('jet')
+caxis([0 1e4])
+title('Day=1')
+m_grid('xtick',12,'tickdir','out','ytick',[-50 -60 -70],'linest','-');
+m_coast('patch',[.7 .7 .7],'edgecolor','k');
+%print('-dpng',[fpath 'advec_test_' cname '_1_Spoleproj.png'])
+
+figure(27)
+m_proj('stereographic','lat',-90,'long',30,'radius',50);
+m_pcolor(geolon_t,geolat_t,B2);
+shading flat
+colorbar
+colormap('jet')
+caxis([0 1e4])
+title('Day=73')
+m_grid('xtick',12,'tickdir','out','ytick',[-50 -60 -70],'linest','-');
+m_coast('patch',[.7 .7 .7],'edgecolor','k');
+%print('-dpng',[fpath 'advec_test_' cname '_2_Spoleproj.png'])
+
+figure(28)
+m_proj('stereographic','lat',-90,'long',30,'radius',50);
+m_pcolor(geolon_t,geolat_t,B3);
+shading flat
+colorbar
+colormap('jet')
+caxis([0 1e4])
+title('Day=146')
+m_grid('xtick',12,'tickdir','out','ytick',[-50 -60 -70],'linest','-');
+m_coast('patch',[.7 .7 .7],'edgecolor','k');
+%print('-dpng',[fpath 'advec_test_' cname '_3_Spoleproj.png'])
+
+figure(29)
+m_proj('stereographic','lat',-90,'long',30,'radius',50);
+m_pcolor(geolon_t,geolat_t,B4);
+shading flat
+colorbar
+colormap('jet')
+caxis([0 1e4])
+title('Day=219')
+m_grid('xtick',12,'tickdir','out','ytick',[-50 -60 -70],'linest','-');
+m_coast('patch',[.7 .7 .7],'edgecolor','k');
+%print('-dpng',[fpath 'advec_test_' cname '_4_Spoleproj.png'])
+
+figure(30)
+m_proj('stereographic','lat',-90,'long',30,'radius',50);
+m_pcolor(geolon_t,geolat_t,B5);
+shading flat
+colorbar
+colormap('jet')
+caxis([0 1e4])
+title('Day=365')
+m_grid('xtick',12,'tickdir','out','ytick',[-50 -60 -70],'linest','-');
+m_coast('patch',[.7 .7 .7],'edgecolor','k');
+%print('-dpng',[fpath 'advec_test_' cname '_5_Spoleproj.png'])
+
--- CODE/JW_adv_diff_update.jl
+++ CODE/JW_adv_diff_update.jl
@@ -13,8 +13,8 @@ function sub_diffuse(Bio_in,Ns,A,DX,DY)
 		br = bio[5]
 
 		# Prefactors
-		Alpha = A * DT / (DX[I]^2)
-		Beta  = A * DT / (DY[I]^2)
+		Alpha = A * dtime / (DX[I]^2)
+		Beta  = A * dtime / (DY[I]^2)
 
 		# calculation (2D Forward in Time Centered Scheme)
 		DIFF =  (Alpha*(bu+bd)) + (Beta*(bl+br)) + (bm*(1-(2*Alpha)-(2*Beta)))
@@ -34,26 +34,14 @@ end
 ### ADVECTION ###
 function sub_advection(Bio_in,Nu,U,V,DX,DY,Tp,Tb,tdif,wgt)
 	#Bio_in = .bio; Nu = .nu; DX = GRD["dxtn"]; DY = GRD["dyte"]; U = ENVR.U; V = ENVR.V;
+	Tp = ENVR.Tp; Tb = ENVR.Tb; tdif = Med_f.td; wgt = M_m;
 
 	#! Calc swimming speed (m/d)
-	T = (Tp.*tdif) + (Tb.*(1.0-tdif))
-	Q = ((3.9*wgt.^0.13 * exp(0.149*T)) /100*60*60*24)
-
-	#! Fix velocities - ALREADY FIXED
-	#U[find(U.==minimum(U))] = 0.0
-	#V[find(V.==minimum(V))] = 0.0
+	#T = (Tp.*tdif) + (Tb.*(1.0-tdif))
+	#Q = ((3.9*wgt.^0.13 * exp(0.149*T)) /100*60*60*24)
+	Q = zeros(NX,1)
 
 	#! Find direction to swim in
-	#Nu_n = Nu[GRD["Neigh"]]
-	#data = zeros(NX,Ns,5)
-	#for I = 1:NX
-	#	for J = 1:5
-	#		data[I,:,J] = Nu_n[I][J]
-	#	end
-	#end
-	#M = maximum(data, 3)
-	#IND = ismax = data .== M
-	#KK = find(KK,3)
 	KK = zeros(Int64,NX)
 	LID = zeros(NX,5)
 	for I = 1:NX
@@ -62,9 +50,6 @@ function sub_advection(Bio_in,Nu,U,V,DX,DY,Tp,Tb,tdif,wgt)
 		LID[I,:] = NID - NID[1]
 		KK[I] = findmax([Nu_n[1],Nu_n[2],Nu_n[3],Nu_n[4],Nu_n[5]])[2]
 	end
-	#LID[find(LID)] = 1
-	#LID[:,1] = 1
-
 
 	#! Calculate effective speed
 	u = zeros(NX)
@@ -91,17 +76,14 @@ function sub_advection(Bio_in,Nu,U,V,DX,DY,Tp,Tb,tdif,wgt)
 	v[I5] = V[I5]
 
 	#! Land
-	for J = 1:Ns
-		I1 = find(v.>0. + LID[:,2].==0.)
-		I2 = find(v.<0. + LID[:,3].==0.)
-		I3 = find(u.<0. + LID[:,4].==0.)
-		I4 = find(u.>0. + LID[:,5].==0.)
-
-		v[I1] *= -1
-		v[I2] *= -1
-		u[I3] *= -1
-		u[I4] *= -1
-	end
+	I1 = find(v.>0. + LID[:,2].==0.)
+	I2 = find(v.<0. + LID[:,3].==0.)
+	I3 = find(u.<0. + LID[:,4].==0.)
+	I4 = find(u.>0. + LID[:,5].==0.)
+	v[I1] *= -1
+	v[I2] *= -1
+	u[I3] *= -1
+	u[I4] *= -1
 
 	#! U advection
 	Bio_minus = similar(Bio_in)
@@ -110,7 +92,7 @@ function sub_advection(Bio_in,Nu,U,V,DX,DY,Tp,Tb,tdif,wgt)
 	end
 	for I = 1:NX
 		bio = Bio_in[GRD["Neigh"][I]]
-		UU = u[I] * DT / DX[I]
+		UU = u[I] * dtime / DX[I]
 		BB = bio[1] - ((UU/2)*(bio[3]-bio[2])) -
 							(((UU^2)/2)*(bio[3]-(2*bio[1])+bio[2]))
 		if BB < 0
@@ -126,8 +108,8 @@ function sub_advection(Bio_in,Nu,U,V,DX,DY,Tp,Tb,tdif,wgt)
 		Bio_plus[I] = 0.0
 	end
 	for I = 1:NX
-		bio = Bio_minus[GRD2["Neigh"][I]]
-		VV = v[I] * DT / DY[I]
+		bio = Bio_minus[GRD["Neigh"][I]]
+		VV = v[I] * dtime / DY[I]
 		BB = bio[1] - ((VV/2)*(bio[5]-bio[4])) -
 							(((VV^2)/2)*(bio[5]-(2*bio[1])+bio[4]))
 		if BB < 0
