b1284931c4306ce598f89242956c90af2809886f
Colleen Petrik <colleenpetrik@gmail.com>
2019-01-07 14:16:47 -0600

    Moved Ken's code

--- CODE/KHA_Poem6/calcNu.m
+++
@@ -1,5 +0,0 @@
-function [v, nu] = calcNu(Eavail, mort, param)
-
-v = (Eavail(param.ixFish) ./ param.wc(param.ixFish))';
-vplus = max(0,v);
-nu = (vplus - mort) ./ (1 - param.z(param.ixFish) .^ (1 - mort ./ vplus));
--- CODE/KHA_Poem6/sub_neg.m
+++
@@ -1,7 +0,0 @@
-%%% Negative value checks for interpolated COBALT values
-function bio = sub_neg(bio)
-    ID = (bio < 0);
-    bio(ID) = 0.0;
-    %ID2 = find(isnan(bio))
-    %bio(ID2) = eps()
-end
